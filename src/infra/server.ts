2940b88c46afe0feecebfed3d764fbee76ce25ca
Igor Piedade <ipiedade@Temps-Mac-mini-3.local>
2022-05-09 18:10:30 -0400

    Pushing the API base to the repo.

---
+++ src/infra/server.ts
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (request, response) =>
+  response.json({
+    message: "AssestAPI - Manage your investments and leave it work for you.",
+  })
+);
+
+app.listen(3333);
